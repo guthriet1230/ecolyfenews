4779b98c5b32756f8f3f469bd02ba5825bc33d05
Tyler Guthrie <guthriet1230@gmail.com>
2020-01-20 19:49:42 -0500

    updating gitignore and installing google oauth

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
     res.send({ hi: 'there' })
